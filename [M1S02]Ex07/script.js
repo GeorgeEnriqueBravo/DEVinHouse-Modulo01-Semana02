bd8e45bfbe7bfd3e6d5d0e2daeb943d656ff8877
George Enrique Bravo dos Santos <george_enrique@hotmail.com>
2022-10-13 18:49:59 -0300

    new files

--- [M1S02]Ex07/script.js
+++ [M1S02]Ex07/script.js
@@ -1,15 +1,5 @@
 let data = new Date()
-console.log(data)
 
-let mes = data.getMonth()
-console.log(mes+1)
+let estacao = ""
 
-let dia = data.getDate()
-console.log(dia)
-
-let m = Number(mes)
-let d = Number(dia)
-
-if (m === 12 && d >= 22 || ) {
-
-}
+if (data
